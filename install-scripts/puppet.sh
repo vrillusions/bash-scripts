ef124b65303b6ffc2449bac70ef583d8bd43977e
Todd Eddy <vr@vrillusions.com>
2012-11-06 15:48:53 -0500

    Add puppet install script.

---
+++ install-scripts/puppet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Must be run as root"
+    exit 1
+fi
+
+echo "Installing puppet using puppetlabs ppa"
+
+FILENAME="puppetlabs-release-$(lsb_release -cs).deb"
+wget http://apt.puppetlabs.com/$FILENAME
+dpkg -i $FILENAME
+rm -f $FILENAME
+apt-get update
+apt-get install -y puppet
+
+echo
+echo "puppet is installed"
+
+exit 0
+
