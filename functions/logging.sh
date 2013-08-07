da5f49eae6ad2aef5d6afa1d15eb1ba64a369614
Todd Eddy <vr@vrillusions.com>
2013-08-06 23:29:18 -0400

    Add logging functions.

---
+++ functions/logging.sh
@@ -0,0 +1,36 @@
+#
+# Several function to print out to screen with a date stamp
+#
+# Usage:
+#   source ./log_function.sh
+#
+# Don't have a 'shebang' at top because this should always be sourced from main
+# script. Also doesn't need to be executable.
+#
+# Expected environment variables:
+#   DEBUG - debug function will only print out of this is 'true'
+#
+
+# log "What to log"
+log () {
+    printf "%b\n" "$(date +"%Y-%m-%dT%H:%M:%S%z") $*"
+}
+
+# debug "What to log if verbose is true"
+debug () {
+    if [[ "x$DEBUG" == "xtrue" ]]; then
+        log "$*"
+    fi
+}
+
+# warn "What to log to STDERR but not exit"
+warn () {
+    log "$*" >&2
+}
+
+# fail "Message to print to STDERR before exiting"
+# Purposely don't use warn function just in case you want this to go to STDOUT
+fail () {
+    log "$*" >&2
+    exit 1
+}
