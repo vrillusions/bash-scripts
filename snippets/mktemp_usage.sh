0f54012d5e7c570d86aa263fd9d8c6ef724e84b3
Todd Eddy <vr@vrillusions.com>
2014-07-27 21:02:28 -0400

    Add extra comments on mktemp_usage.sh

--- snippets/mktemp_usage.sh
+++ snippets/mktemp_usage.sh
@@ -11,17 +11,21 @@
 set -e
 set -u
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename $0)"
 
+# BUG: need to test if ~/temp exists
 export TMPDIR=${TMPDIR:-"${HOME}/temp"}
 tmpfile=$(mktemp --tmpdir ${script_name}.XXXXXXXXX)
 echo "Using temp file ${tmpfile}"
 
-trap "rm -f $tmpfile" EXIT
+trap "rm -f ${tmpfile}" EXIT
 
+# Just some example stuff to put in file
 uptime >>"${tmpfile}"
 w >>"${tmpfile}"
 cat "${tmpfile}"
+
+# Note that we are just exiting without removing the tmpfile but it will be
+# handled by trap command at top
 
 exit 0
