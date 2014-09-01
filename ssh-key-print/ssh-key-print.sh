2a9e60b5bb7545aa7ea783b8bf18edb1fe4c9df1
Todd Eddy <vr@vrillusions.com>
2014-09-01 08:12:52 -0400

    ssh-key-print: add type 2 (sha256) dns records

--- ssh-key-print/ssh-key-print.sh
+++ ssh-key-print/ssh-key-print.sh
@@ -15,6 +15,10 @@ log () {
 }
 
 
+readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly script_name="$(basename $0)"
+
+
 ### Option Handling
 while getopts ":h" opt; do
     case $opt in
@@ -29,7 +33,7 @@ while getopts ":h" opt; do
         printf " %6s - %s\n" "SSH" "fingerprint as ssh will show it upon first connect"
         printf " %6s - %s\n" "SHA1" "SHA1 hash of key"
         printf " %6s - %s\n" "SHA256" "SHA256 hash of key"
-        printf " %6s - %s\n" "DNS" "SSHFP record to place in dns"
+        printf " %6s - %s\n" "DNS" "SSHFP records to place in dns"
         exit 0
         ;;
     \?)
@@ -39,6 +43,10 @@ while getopts ":h" opt; do
     esac
 done
 shift `expr $OPTIND - 1`
+
+
+tmpfile=$(mktemp --tmpdir ${script_name}.XXXXXXXXXX)
+trap "rm -f ${tmpfile}" EXIT
 
 
 # Given a key type will output the dns type value
@@ -78,6 +86,8 @@ process_key () {
     local key_type
     local sha1
     local sha256
+    local dns_sha1
+    local dns_sha256
 
     if [[ $# -ne 1 ]]; then
         log "(process_key) ERROR: requires exactly one argument"
@@ -96,11 +106,16 @@ process_key () {
         key_type="$(echo "${keygen_info}" | cut -d' ' -f5 | tr -d '()')"
         sha1="$(echo -n "${key_bytes}" | openssl sha1 | awk '{print $2}')"
         sha256="$(echo -n "${key_bytes}" | openssl sha256 | awk '{print $2}')"
+        dns_sha1="$(hostname -f). 86400 IN SSHFP $(get_dns_type ${key_type}) 1 ${sha1}"
+        dns_sha256="$(hostname -f). 86400 IN SSHFP $(get_dns_type ${key_type}) 2 ${sha256}"
+        echo "${dns_sha1}" >>"${tmpfile}"
+        echo "${dns_sha256}" >>"${tmpfile}"
         printf "\n%s\n" "${key_type}"
         printf " %6s: %s\n" "SSH" "${fingerprint}"
         printf " %6s: %s\n" "SHA1" "${sha1}"
         printf " %6s: %s\n" "SHA256" "${sha256}"
-        printf " %6s: %s\n" "DNS" "$(hostname -f). 86400 IN SSHFP $(get_dns_type ${key_type}) 1 ${sha1}"
+        printf " %6s: %s\n" "DNS" "${dns_sha1}"
+        printf " %6s  %s\n" ""    "${dns_sha256}"
     fi
 }
 
@@ -110,6 +125,8 @@ printf "%s\n" "SSH KEYS FOR HOST ${hostupper}"
 process_key "/etc/ssh/ssh_host_dsa_key.pub"
 process_key "/etc/ssh/ssh_host_rsa_key.pub"
 process_key "/etc/ssh/ssh_host_ecdsa_key.pub"
+printf "\n%s\n" "ALL THE DNS RECORDS IN ONE BLOCK"
+cat "${tmpfile}"
 
 exit 0
 
