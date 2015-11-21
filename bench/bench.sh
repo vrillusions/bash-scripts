2e3a9820eb8055783279783520a7a15e2a821895
Todd Eddy <vr@vrillusions.com>
2015-11-21 15:43:14 -0500

    bench: v2.0
    - format output a little better (reason for bump in major version in case it
      breaks someone's scripts)
    - cleaned up script so now it passes shellcheck
    - fixed the Linode speedtest URLs
    - create functions for testing speed and ping which makes it easier to
      add/remove places.

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# src: https://raw.github.com/vrillusions/bash-scripts/master/bench/bench.sh
+# src: https://raw.githubusercontent.com/vrillusions/bash-scripts/master/bench/bench.sh
+# home: https://github.com/vrillusions/bash-scripts (in bench subfolder)
+#
 # runs various benchmarks on a server. Commonly done to test VPS machines
 # improvements include better formatting and make it easier to add more
 # download sites.  Also CPU frequency should be taken off the fastest core
@@ -13,86 +15,166 @@
 # BENCH_OPENSSL_EC - run speed tests against a couple elliptic curve algorithms.
 #   Anything other than 'true' will skip them (may be needed on older systems).
 #   Default is 'true'
-#
-# TODO: make in python since bash is so clunky to do advanced stuff in
 
-_version='1.1'
+_version='2.0'
 
-# make sure we have everything we need
-# TODO: I'm sure there's a better way to do this
-CURL=$(which curl) || eval "echo 'Please install curl'; exit 1"
-CURL=$(which openssl) || eval "echo 'Please install openssl'; exit 1"
 
-# Gather info
-CPU_NAME=$(cat /proc/cpuinfo | grep "model name" | uniq | sed -e "s/^model name\s*: //")
-CPU_CORES=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-CPU_FREQ=$(cat /proc/cpuinfo | grep "cpu MHz" | sort| uniq | head -1 | sed -e "s/^cpu MHz\s*: \(.*\)/\1 MHz/")
-RAM=$(free -m | grep "Mem" | awk '{print $2 " MB"}')
-SWAP=$(free -m | grep "Swap" | awk '{print $2 " MB"}')
-UPTIME=$(uptime | sed -e "s/.*up \(.*\), .* users.*/\1/")
+# -- setup some functions --
+# usage: log "What you want to log"
+log () {
+    # Usually I include a timestamp but not needed here
+    # Still have this function since printf is theoretically better than echo
+    #printf "%b\n" "$(date +"%Y-%m-%dT%H:%M:%S%z") $*"
+    printf "%b\n" "$*"
+}
 
-echo "bench.sh v${_version}"
+# usage: logerror "What to log to stderr"
+logerror () {
+    log "$*" >&2
+}
+
+# usage: logfatal "What to log to stderr and exit"
+logfatal () {
+    logerror "$*"
+    exit 1
+}
+
+# usage: command_exists "command_name" || echo "the command wasn't found"
+command_exists () {
+    local command_name
+    command_name="${1-}"
+
+    if [[ "${command_name}" == "" ]]; then
+        echo "ERROR: command_exists(): no command specified" >&2
+        return 1
+    fi
+
+    if command -v "${command_name}" 1>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# usage: speedtest "Friendly name" "url"
+# Keep the friendly name under 30 characters so formatting stays pretty
+speedtest () {
+    if [[ $# -ne 2 ]]; then
+        echo "ERROR: speedtest(): incorrect number of arguments specified"
+        return 1
+    fi
+    local friendly_name
+    local url
+    local speedtest_result
+    friendly_name="$1"
+    url="$2"
+
+    # The log function adds a linebreak at the end we don't want so call printf
+    # directly
+    printf "%30b: " "${friendly_name}"
+    speedtest_result="$(($(curl -s -o /dev/null --write-out %\{speed_download\} \
+        "${url}" | cut -d'.' -f1) / 1000)) KB/sec"
+    printf "%-15b\n" "${speedtest_result}"
+}
+
+# usage: pingtest "Friendly name" "address"
+pingtest () {
+    if [[ $# -ne 2 ]]; then
+        echo "ERROR: pingtest(): incorrect number of arguments specified"
+        return 1
+    fi
+    local friendly_name
+    local server
+    friendly_name="$1"
+    server="$2"
+
+    log "${friendly_name}"
+    ping -q -c5 "${server}" | tail -3
+    log
+}
+
+
+
+# -- make sure we have needed commands --
+command_exists "curl" || logfatal "Please install curl"
+command_exists "openssl" || logfatal "Please install OpenSSL"
+
+
+# -- gather info --
+cpu_name="$(grep "model name" /proc/cpuinfo \
+    | uniq \
+    | sed -e "s/^model name\s*: //")"
+cpu_cores="$(grep -c "processor" /proc/cpuinfo)"
+cpu_freq="$(grep "cpu MHz" /proc/cpuinfo \
+    | sort \
+    | uniq \
+    | head -1 \
+    | sed -e "s/^cpu MHz\s*: \(.*\)/\1 MHz/")"
+ram="$(free -m \
+    | grep "Mem" \
+    | awk '{print $2 " MB"}')"
+swap="$(free -m \
+    | grep "Swap" \
+    | awk '{print $2 " MB"}')"
+uptime="$(uptime \
+    | sed -e 's/^\W//')"
+
+
+log "bench.sh v${_version}"
 date
-echo
-echo "CPU model:            $CPU_NAME"
-echo "Number of cores:      $CPU_CORES"
-echo "CPU frequency:        $CPU_FREQ"
-echo "Total amount of ram:  $RAM"
-echo "Total amount of swap: $SWAP"
-echo "System uptime:        $UPTIME"
-echo
+log
+log "CPU model:            ${cpu_name}"
+log "Number of cores:      ${cpu_cores}"
+log "CPU frequency:        ${cpu_freq}"
+log "Total amount of ram:  ${ram}"
+log "Total amount of swap: ${swap}"
+log "System uptime:        ${uptime}"
+log
 
-echo -n "Beginning I/O test:   "
-echo$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
-echo
 
-echo "Beginning ping tests:"
-echo "google.com, US"
-ping -q -c5 google.com | tail -3
-echo
-echo "linode.com, US"
-ping -q -c5 linode.com | tail -3
-echo
-echo "Rakuten, Japan"
-ping -q -c5 rakuten.co.jp | tail -3
-echo
-echo "BBC, UK"
-ping -q -c5 bbc.co.uk | tail -3
-echo
-echo "Whirlpool Broadband News, Australia"
-ping -q -c5 whirlpool.net.au | tail -3
-echo
+printf "%b" "Beginning I/O test:  "
+log "$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync \
+    && rm -f test_$$ ) 2>&1 \
+    | awk -F, '{io=$NF} END { print io}' )"
+log
 
-# now for downloads
-# first off load up our array of sites, format is:
-# FriendlyName|URL
-TEST_FILES[0]="CacheFly|http://cachefly.cachefly.net/100mb.test"
-TEST_FILES[1]="Linode, Atlanta GA, US|http://atlanta1.linode.com/100MB-atlanta.bin"
-TEST_FILES[2]="Linode, Dallas, TX, US|http://dallas1.linode.com/100MB-dallas.bin"
-TEST_FILES[3]="Linode, Tokyo, JP|http://tokyo1.linode.com/100MB-tokyo.bin"
-TEST_FILES[4]="Linode, London, UK|http://london1.linode.com/100MB-london.bin"
-TEST_FILES[5]="Leaseweb, Haarlem, NL|http://mirror.leaseweb.com/speedtest/100mb.bin"
-TEST_FILES[6]="Softlayer, Singapore|http://speedtest.sng01.softlayer.com/downloads/test100.zip"
-TEST_FILES[7]="Softlayer, Seattle, WA, US|http://speedtest.sea01.softlayer.com/downloads/test100.zip"
-TEST_FILES[8]="Softlayer, San Jose, CA|http://speedtest.sjc01.softlayer.com/downloads/test100.zip"
-TEST_FILES[9]="Softlayer, Washington, DC|http://speedtest.wdc01.softlayer.com/downloads/test100.zip"
-TEST_FILES[10]="OVH, France|http://proof.ovh.net/files/100Mio.dat"
 
-echo "Beginning download speed tests:"
-for TEST_FILE in "${TEST_FILES[@]}"; do
-    NAME=$(echo $TEST_FILE | cut -d'|' -f1)
-    URL=$(echo $TEST_FILE | cut -d'|' -f2)
-    echo -n "$NAME:  "
-    echo "$(($(curl -s -o /dev/null --write-out %{speed_download} $URL | cut -d'.' -f1) / 1000)) KB/sec"
-done
-echo
+log "Beginning ping tests:"
+pingtest "google.com, USA" "google.com"
+pingtest "linode.com, USA" "linode.com"
+pingtest "Rakuten, JPN" "rakuten.co.jp"
+pingtest "BBC, GBR" "bbc.co.uk"
+pingtest "Whirlpool Broadband News, AUS" "whirlpool.net.au"
+
+
+log "Beginning download speed tests:"
+speedtest "CacheFly" "http://cachefly.cachefly.net/100mb.test"
+speedtest "Linode, Newark, NJ, USA" "http://speedtest.newark.linode.com/100MB-newark.bin"
+speedtest "Linode, Atlanta, GA, USA" "http://speedtest.atlanta.linode.com/100MB-atlanta.bin"
+speedtest "Linode, Dallas, TX, USA" "http://speedtest.dallas.linode.com/100MB-dallas.bin"
+speedtest "Linode, Tokyo, JPN" "http://speedtest.tokyo.linode.com/100MB-tokyo.bin"
+speedtest "Linode, London, GBR" "http://speedtest.london.linode.com/100MB-london.bin"
+speedtest "Leaseweb, Haarlem, NLD" "http://mirror.leaseweb.com/speedtest/100mb.bin"
+speedtest "Softlayer, SGP" "http://speedtest.sng01.softlayer.com/downloads/test100.zip"
+speedtest "Softlayer, Seattle, WA, USA" "http://speedtest.sea01.softlayer.com/downloads/test100.zip"
+speedtest "Softlayer, San Jose, CA, USA" "http://speedtest.sjc01.softlayer.com/downloads/test100.zip"
+speedtest "Softlayer, Washington, DC, USA" "http://speedtest.wdc01.softlayer.com/downloads/test100.zip"
+speedtest "OVH, FRA" "http://proof.ovh.net/files/100Mio.dat"
+speedtest "OVH, Quebec, CAN" "http://proof.ovh.ca/files/100Mio.dat"
+log
+
 
 if [[ "${BENCH_OPENSSL_EC:-true}" == 'true' ]]; then
     ciphers='sha256 aes-128-cbc aes-256-cbc ecdsap256 ecdhp256'
 else
     ciphers='sha256 aes-128-cbc aes-256-cbc'
 fi
-echo "Beginning CPU tests (won't see output for 1 or 2 minutes):"
+log "Beginning CPU tests (won't see output for 1 or 2 minutes):"
+# ${ciphers} can't be enclosed in quotes so disable shellcheck warning about it
+#shellcheck disable=SC2086
 openssl speed ${ciphers} 2>/dev/null | tail -n +6
+log
+
+log "Finished benchmark. Have a nice day!"
 
 exit 0
